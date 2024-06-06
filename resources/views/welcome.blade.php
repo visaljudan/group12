0d8e03d78e823ebe8582c08f17b176cc752e6f89
visaljudan <visal.nls2003@gmail.com>
2024-06-06 09:35:29 +0700

    update for deploy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Rock paper scissors Game</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Styles -->
-        @vite('resources/css/app.css')
+    <title>Rock paper scissors Game</title>
 
-        <!-- Scripts -->
-        @livewireStyles
-    </head>
-    <body class="antialiased">
-        <livewire:game-field />
+    <!-- Styles -->
+    @vite('resources/css/app.css')
 
-        @livewireScripts
-    </body>
+    <!-- Scripts -->
+    @livewireStyles
+</head>
+
+<body class="antialiased">
+    <livewire:game-field />
+
+    @livewireScripts
+</body>
+
 </html>
