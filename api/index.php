ee51b6950782dc7e68e4f6a10616eea35eec467e
visaljudan <visal.nls2003@gmail.com>
2024-06-06 10:03:47 +0700

    update

--- api/index.php
+++ api/index.php
@@ -1,3 +1,11 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
-require __DIR__ . "/../public/index.php";
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$request = Illuminate\Http\Request::capture();
+$response = $kernel->handle($request);
+
+$response->send();
+$kernel->terminate($request, $response);
